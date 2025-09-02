e71747c5b30878262a869757ebeea103f2bf2aea
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-09-02 20:03:23 +0800

    Add file list and `pathElement` to refresh button
    Refresh button is responsible for querying the server
    for a list of filepaths given the directory path and sending
    them to the file list object.

--- server/main/static/main/js/RefreshButton.js
+++ server/main/static/main/js/RefreshButton.js
@@ -1,4 +1,5 @@
 import {ActionButton} from "./ActionButton.js";
+import { FileList } from "./FileList.js";
 
 /**
  * Class to define behaviour for refresh button responsible for
@@ -10,10 +11,18 @@ export class RefreshButton extends ActionButton
      * Constructor.
      * 
      * @param {HTMLElement} htmlElement HTML element of refresh button.
+     * @param {FileList} fileList FileList object.
+     * @param {HTMLElement} pathElement HTML element of directory path.
      */
-    constructor(htmlElement)
+    constructor(htmlElement, fileList, pathElement)
     {
         super(htmlElement);
+
+        // File list element to send filepaths to.
+        this.fileList = fileList;
+
+        // Directory to path.
+        this.pathElement = pathElement;
 
         // Add event listener for mouse click.
         this.htmlElement.addEventListener("click", () => this.actionOnClick());
--- server/main/static/main/js/main.js
+++ server/main/static/main/js/main.js
@@ -1,13 +1,21 @@
 import { RefreshButton } from "./RefreshButton.js";
+import { FileList } from "./FileList.js";
 
 /**
  * Main function to call for overall HTML page area.
  */
 function main()
 {
+    // Get directory path element.
+    let pathElement = document.getElementById("path-textbox");
+
+    // Create file list object.
+    let fileListElement = document.getElementById("file-list");
+    let fileListObj = new FileList(fileListElement);
+
     // Create refresh button object.
     let refreshButtonElement = document.getElementById("refresh-button");
-    let refreshButtonObj = new RefreshButton(refreshButtonElement);
+    let refreshButtonObj = new RefreshButton(refreshButtonElement, fileListObj, pathElement);
 }
 
 /**
