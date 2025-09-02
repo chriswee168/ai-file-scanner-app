18fabee78b2a155dd4bb86d7a694c78cc95bab12
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-09-02 17:18:23 +0800

    Create "main.js" for managing HTML page behaviour

---
+++ server/main/static/main/js/main.js
@@ -0,0 +1,14 @@
+/**
+ * Main function to call for overall HTML page area.
+ */
+function main()
+{
+    
+}
+
+/**
+ * Call main function only when HTML page has been loaded.
+ */
+document.addEventListener("DOMContentLoaded", () => {
+    main();
+});
