4afd28afd89b8538ace48b0979af7ee644dc8296
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-09-11 00:40:04 +0800

    Add type attribute to metadata
    Indicate whether an entry is a file or folder in selection
    box.

--- server/main/custom_funcs/recursive_dir_search.py
+++ server/main/custom_funcs/recursive_dir_search.py
@@ -9,11 +9,13 @@ def recurse_dir(path: str, indent_text: str, filepaths: list[str], metadatas: li
         # Edit entry name to display if directory.
         if os.path.isdir(abs_path):
             name = f"[{name}]"
+            entry_type = "folder"
         elif os.path.isfile(abs_path):
-            pass
+            entry_type = "file"
 
         metadata = {
             "name": os.path.basename(abs_path),
+            "type": entry_type,
             "absolute_path": abs_path,
             "size": os.path.getsize(abs_path),
             "last_created": ctime(os.path.getctime(abs_path)),
