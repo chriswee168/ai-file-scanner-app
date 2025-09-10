962d73a849bd53aa794f34393348ca4373bb4b57
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-09-10 22:58:12 +0800

    Add recursive directory search function
    Uses recursion to traverse the target directory
    and obtain all subdirectories and files. Allows
    better formatting of file selection box.

---
+++ server/main/custom_funcs/recursive_dir_search.py
@@ -0,0 +1,42 @@
+import os
+from time import ctime
+
+# Recursively traverse all sub directories and files.
+def recurse_dir(path: str, indent_text: str, filepaths: list[str], metadatas: list[dict]):
+    for name in os.listdir(path):
+        abs_path = os.path.join(path, name)
+
+        # Edit entry name to display if directory.
+        if os.path.isdir(abs_path):
+            name = f"[{name}]"
+        elif os.path.isfile(abs_path):
+            pass
+
+        metadata = {
+            "name": os.path.basename(abs_path),
+            "absolute_path": abs_path,
+            "size": os.path.getsize(abs_path),
+            "last_created": ctime(os.path.getctime(abs_path)),
+            "last_accessed": ctime(os.path.getatime(abs_path)),
+            "last_modified": ctime(os.path.getmtime(abs_path))
+        }
+
+        filepaths.append(indent_text + name)
+        metadatas.append(metadata)
+        
+        if os.path.isdir(abs_path):
+            recurse_dir(abs_path, indent_text + "| ", filepaths, metadatas)
+        elif os.path.isfile(abs_path):
+            pass
+
+# Wrapper function for recursive directory traversal.
+def search_dir(root_path: str) -> tuple[list[str], list[dict]]:
+    # List of filepaths to return.
+    filepaths: list[str] = []
+    metadatas: list[dict] = []
+    
+    # Recursively search directory.
+    indent_text = ""
+    recurse_dir(root_path, indent_text, filepaths, metadatas)
+
+    return filepaths, metadatas
