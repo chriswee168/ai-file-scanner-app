944a3c65194beba1288bbd9081cfc3054da1b6ab
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-09-12 11:32:27 +0800

    Change button colour on mouseover and leave
    Allow buttons to have responsiveness when moving
    mouse cursor over them. Indicates that they can be
    clicked.

--- server/main/static/main/js/ActionButton.js
+++ server/main/static/main/js/ActionButton.js
@@ -14,8 +14,17 @@ export class ActionButton
         this.available = false;
         this.htmlElement = htmlElement;
 
-        this.availableStyle = ["rgb(8, 98, 0)", "rgb(255, 255, 255)"];
-        this.unavailableStyle = ["rgb(98, 0, 0)", "rgb(255, 255, 255)"];
+        // [backgroundColor, textColor, mouseOverBackgroundColor].
+        this.availableStyle = [
+            "rgb(8, 98, 0)", "rgb(255, 255, 255)", "rgb(5, 60, 0)"
+        ];
+        this.unavailableStyle = [
+            "rgb(98, 0, 0)", "rgb(255, 255, 255)", "rgb(63, 0, 0)"
+        ];
+
+        // Listen for mouser over and leave events.
+        this.htmlElement.addEventListener("mouseover", () => this.onMouseOver());
+        this.htmlElement.addEventListener("mouseleave", () => this.onMouseLeave());
     }
 
     /**
@@ -46,5 +55,37 @@ export class ActionButton
             this.htmlElement.style.color = this.unavailableStyle[1];
         }
         this.available = available;
+    }
+
+    /**
+     * Change background colour on mouse over event. Use colour depending
+     * on availability state.
+     */
+    onMouseOver()
+    {
+        if (this.available)
+        {
+            this.htmlElement.style.backgroundColor = this.availableStyle[2];
+        }
+        else
+        {
+            this.htmlElement.style.backgroundColor = this.unavailableStyle[2];
+        }
+    }
+
+    /**
+     * Reset the background colour on mouse leave event. Use colour depending
+     * on availability state.
+     */
+    onMouseLeave()
+    {
+        if (this.available)
+        {
+            this.htmlElement.style.backgroundColor = this.availableStyle[0];
+        }
+        else
+        {
+            this.htmlElement.style.backgroundColor = this.unavailableStyle[0];
+        }
     }
 }
