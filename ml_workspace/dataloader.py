19db715943b2e1741a15750b47a8457d38cc99a1
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-09-27 11:33:57 +0800

    Revert "Modify dataset creator function"
    This reverts commit 36d51bd3e82dd6de86b6f5d5727b70a04cc47fa3.

--- ml_workspace/dataloader.py
+++ ml_workspace/dataloader.py
@@ -3,30 +3,21 @@ import torch
 from torch import Tensor
 
 def create_byte_dataset(
-    dir_path: str, output_dir: str, chunk_size: int, stride: int, 
-    max_samples_per_class: int):
-    
-    # Create target directories to write to.
-    if not os.path.exists(os.path.join(output_dir, "inputs")):
-        os.makedirs(os.path.join(output_dir, "inputs"))
-    
-    if not os.path.exists(os.path.join(output_dir, "outputs")):
-        os.makedirs(os.path.join(output_dir, "outputs"))
+    dir_path: str, chunk_size: int, stride: int, 
+    max_samples_per_class: int) -> list[tuple[Tensor, Tensor]]:
 
+    # Contains the number of training examples for each class.
+    dataset: list[tuple[Tensor, Tensor]] = []
+    
     # The directory names in dataset.
     classes = ["clean", "malicious"]
-
-    dataset_size = 0
 
     for c, cat in enumerate(classes):
         classpath = os.path.join(dir_path, cat)
 
         class_count = 0
 
-        for k, file in enumerate(os.listdir(classpath)):
-            sub_dataset_x: list[Tensor] = []
-            sub_dataset_y: list[Tensor] = []
-
+        for file in os.listdir(classpath):
             # Get full byte sequence of file as uint8.
             filepath = os.path.join(classpath, file)
             with open(filepath, "rb") as f:
@@ -45,8 +36,9 @@ def create_byte_dataset(
                     # Only add chunks that are the same length of chunk size.
                     # (Usually the last chunk is almost always shorter.)
                     if len(byte_seq_chunk) == chunk_size:
-                        sub_dataset_x.append(byte_seq_chunk)
-                        sub_dataset_y.append(torch.FloatTensor([[c]]))
+                        dataset.append(
+                            (byte_seq_chunk, torch.FloatTensor([[c]]))
+                        )
                         
                         chunk_counter += 1
                         class_count += 1
@@ -55,22 +47,7 @@ def create_byte_dataset(
             
             if class_count < max_samples_per_class:
                 print("\n", end="")
-            
-            # If samples in sub dataset.
-            if sub_dataset_x:
-
-                # Remove duplicates.
-                sub_dataset_x = torch.stack(sub_dataset_x, dim=0)
-                sub_dataset_x_nodup = torch.unique(sub_dataset_x, dim=0)
-                sub_dataset_y = torch.stack(sub_dataset_y, dim=0)
-                sub_dataset_y = sub_dataset_y[:len(sub_dataset_x_nodup)]
-
-                # Save x and y subset tensors to file.
-                torch.save(sub_dataset_x_nodup, os.path.join(output_dir, "inputs", f"{k}.pt"))
-                torch.save(sub_dataset_y, os.path.join(output_dir, "outputs", f"{k}.pt"))
-
-                # Update full database size.
-                dataset_size += len(sub_dataset_x_nodup)
-
     
-    print(f"\nDataset size: {dataset_size}")
+    print(f"\nCurrent dataset size: {len(dataset)}")
+
+    return dataset
